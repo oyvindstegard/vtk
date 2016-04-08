746cd16afe44c8b1a83a904ffc0c9b7b36fdf1ea
Nils Fredrik Gjerull <nils@gjerull.net>
2016-04-08 13:08:20 +0200

    [VTK-4220] Simplify SQL upgrade script.

--- src/main/sql/upgrades/66-oracle.sql
+++ src/main/sql/upgrades/66-oracle.sql
@@ -8,9 +8,7 @@ select extra_prop_entry_seq_pk.nextval,
        null,
        null
 from vortex_resource
-where resource_id in
-    (select distinct(resource_id) from extra_prop_entry
-        where is_collection = 'Y')
+where is_collection = 'Y'
 and resource_id not in
     (select distinct(resource_id) from extra_prop_entry
         where name_space is null and name = 'recursive-listing');
