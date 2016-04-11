3b13c8d0f0f55101c7f34817ac8ae98b88e8f673
Gorm Paulsen <gorm.paulsen@usit.uio.no>
2016-04-11 14:41:29 +0200

    [VTK-4220] Migrate listing properties
    Move 'recursive-listing', 'recursive-listing-subfolders' to
    collection (default) namespace. Remove 'authors' property.

--- src/main/sql/upgrades/66-oracle.sql
+++ src/main/sql/upgrades/66-oracle.sql
@@ -1,3 +1,5 @@
+-- Property 'recursive-listing' is now mandatory for collections:
+
 insert into extra_prop_entry
 select extra_prop_entry_seq_pk.nextval,
        resource_id,
@@ -13,3 +15,23 @@ where is_collection = 'Y'
 and resource_id not in
     (select distinct(resource_id) from extra_prop_entry
         where name_space is null and name = 'recursive-listing');
+
+
+-- The 'authors' property is multi-valued, and therefore has to be
+-- removed (we don't support dead multi-value properties):
+
+delete from extra_prop_entry where name_space is null and name = 'authors';
+
+-- These properties have been promoted to the 'collection' resource type (null namespace):
+
+update extra_prop_entry set name_space = null 
+where name = 'recursive-listing' and name_space = 'http://www.uio.no/resource-types/article-listing';
+
+update extra_prop_entry set name_space = null 
+where name = 'recursive-listing' and name_space = 'http://www.uio.no/resource-types/blog-listing';
+
+update extra_prop_entry set name_space = null 
+where name = 'recursive-listing' and name_space = 'http://www.uio.no/resource-types/event-listing';
+
+update extra_prop_entry set name_space = null 
+where name = 'recursive-listing-subfolders' and name_space = 'http://www.uio.no/resource-types/article-listing';
--- src/main/sql/upgrades/66-postgresq.sql
+++ src/main/sql/upgrades/66-postgresq.sql
@@ -1,3 +1,5 @@
+-- Property 'recursive-listing' is now mandatory for collections:
+
 insert into extra_prop_entry
 select nextval('extra_prop_entry_seq_pk'),
        resource_id,
@@ -12,3 +14,22 @@ where is_collection = 'Y'
 and resource_id not in
     (select distinct(resource_id) from extra_prop_entry
         where name_space is null and name = 'recursive-listing');
+
+-- The 'authors' property is multi-valued, and therefore has to be
+-- removed (we don't support dead multi-value properties):
+
+delete from extra_prop_entry where name_space is null and name = 'authors';
+
+-- These properties have been promoted to the 'collection' resource type (null namespace):
+
+update extra_prop_entry set name_space = null 
+where name = 'recursive-listing' and name_space = 'http://www.uio.no/resource-types/article-listing';
+
+update extra_prop_entry set name_space = null 
+where name = 'recursive-listing' and name_space = 'http://www.uio.no/resource-types/blog-listing';
+
+update extra_prop_entry set name_space = null 
+where name = 'recursive-listing' and name_space = 'http://www.uio.no/resource-types/event-listing';
+
+update extra_prop_entry set name_space = null 
+where name = 'recursive-listing-subfolders' and name_space = 'http://www.uio.no/resource-types/article-listing';
