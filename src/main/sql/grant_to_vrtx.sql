0b56eda6f8382b0b8891dc85a1703fbf05faf847
oddmund <oddmund.mogedal@usit.uio.no>
2016-07-27 13:38:25 +0200

    Removed grants to nonexisting tables, added new ones, and added grant to sequences.

--- src/main/sql/grant_to_vrtx.sql
+++ src/main/sql/grant_to_vrtx.sql
@@ -3,8 +3,12 @@ grant delete, select, insert, update on REVISION_ACL_ENTRY to vrtx;
 grant delete, select, insert, update on ACL_ENTRY to vrtx;                      
 grant delete, select, insert, update on ACTION_TYPE to vrtx;                    
 grant delete, select, insert, update on EXTRA_PROP_ENTRY to vrtx;               
-grant delete, select, insert, update on LOCK_TYPE to vrtx;                      
-grant delete, select, insert, update on PARENT_CHILD to vrtx;                   
 grant delete, select, insert, update on VORTEX_LOCK to vrtx;                    
 grant delete, select, insert, update on VORTEX_RESOURCE to vrtx;                
 grant delete, select, insert, update on DELETED_RESOURCE to vrtx;
+grant delete, select, insert, update on VORTEX_TMP to vrtx;  
+grant delete, select, insert, update on  PROP_TYPE to vrtx;  
+grant delete, select, insert, update on  CHANGELOG_ENTRY to vrtx;  
+grant delete, select, insert, update on  VORTEX_COMMENT to vrtx;  
+
+grant usage, select on all sequences in schema public to vrtx;
