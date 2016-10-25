1ad25846cb0f2fcc6e24ae1d802974b50ae4247f
Gorm Paulsen <gorm.paulsen@usit.uio.no>
2016-10-25 15:21:28 +0200

    [VTK-4699] Add property 'audit-log'
    Optional property (JSON/binary) that stores a list of operations that
    are performed on the resource

---
+++ src/main/java/vtk/repository/resourcetype/property/AuditLogEvaluator.java
@@ -0,0 +1,88 @@
+/* Copyright (c) 2016, University of Oslo, Norway
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ *  * Neither the name of the University of Oslo nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package vtk.repository.resourcetype.property;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.time.FastDateFormat;
+
+import vtk.repository.Property;
+import vtk.repository.PropertyEvaluationContext;
+import vtk.repository.PropertyEvaluationContext.Type;
+import vtk.repository.resourcetype.LatePropertyEvaluator;
+import vtk.util.text.Json;
+
+public class AuditLogEvaluator implements LatePropertyEvaluator {
+
+    @Override
+    public boolean evaluate(Property property, PropertyEvaluationContext ctx)
+            throws PropertyEvaluationException {
+        
+        boolean exists = ctx.getOriginalResource().getProperty(property.getDefinition()) != null;
+        if (ctx.getEvaluationType() == Type.SystemPropertiesChange) {
+            return exists;
+        }
+        
+        List<Object> log = null;
+        if (property.isValueInitialized()) {
+            Json.MapContainer object = property.getJSONValue();
+            log = object.optArrayValue("events", new Json.ListContainer());
+        }
+        if (log == null) log = new ArrayList<>();
+        
+        Json.MapContainer entry = new Json.MapContainer();
+        entry.put("type", ctx.getEvaluationType().toString());
+        entry.put("principal", ctx.getPrincipal().getQualifiedName());
+        entry.put("time", FastDateFormat.getInstance("yyyyMMdd HH:mm:ss").format(ctx.getTime()));
+
+        switch (ctx.getEvaluationType()) {
+            case NameChange:
+                entry = nameChangeInfo(entry, ctx);
+                break;
+            default:
+                break;
+        }
+        log.add(entry);
+
+        Json.MapContainer value = new Json.MapContainer();
+        value.put("events", log);
+        property.setJSONValue(value);
+        return true;
+    }
+    
+    private Json.MapContainer nameChangeInfo(Json.MapContainer entry, PropertyEvaluationContext ctx) {
+        entry.put("from_uri", ctx.getOriginalResource().getURI());
+        entry.put("to_uri", ctx.getNewResource().getURI());
+        return entry;
+    }
+
+}
