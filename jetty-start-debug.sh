5b9611d41cbc7e7c46a1d873307459fdff36452f
Nils Fredrik Gjerull <n.f.gjerull@usit.uio.no>
2017-03-03 13:58:12 +0100

    WIP

--- jetty-start-debug.sh
+++ jetty-start-debug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-MAVEN_OPTS="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=4000,suspend=n" mvn jetty:run
+MAVEN_OPTS="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=4000,suspend=n" mvn jetty:run -D maven.compiler.useIncrementalCompilation=false
