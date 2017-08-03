feb756a3fef3488797aea345129f31d115552322
Øyvind Hatland <oyvind.hatland@usit.uio.no>
2017-08-03 14:22:59 +0200

    VTK-5111 - no working, use the autocomplete service to provide it instead

--- src/main/java/vtk/web/AjaxEditorController.java
+++ src/main/java/vtk/web/AjaxEditorController.java
@@ -56,7 +56,6 @@ import org.springframework.web.servlet.support.RequestContextUtils;
 import vtk.repository.Path;
 import vtk.repository.Repository;
 import vtk.repository.Resource;
-import vtk.repository.store.PrincipalMetadata;
 import vtk.resourcemanagement.StaticResourceResolver;
 import vtk.security.Principal;
 import vtk.text.tl.Context;
@@ -135,10 +134,6 @@ public class AjaxEditorController implements Controller {
         Repository repository = rc.getRepository();
         String token = rc.getSecurityToken();
         Principal p = rc.getPrincipal();
-        
-        PrincipalMetadata pm = p.getMetadata();
-        Object sn = pm.getValue("surname");
-        Object fn = pm.getValue("firstName");
 
         Resource resource = repository.lock(
                 token,
@@ -162,8 +157,6 @@ public class AjaxEditorController implements Controller {
         ).perform());
         model.put("resourceURI", resource.getURI());
         model.put("userName", p.getName());
-        model.put("userSurName", (sn != null ? (String) sn : ""));
-        model.put("userFirstName", (fn != null ? (String) fn : ""));
         model.put("userDescription", p.getDescription());
         model.put("userUrl", p.getURL());
         model.put("locale", LocaleHelper.getPreferredLang(locale));
