72074e4aa3d0b0804d764636c55a13b513301bec
Øyvind Stegard <oyvind.stegard@usit.uio.no>
2017-08-03 14:29:48 +0200

    VShell: implement "repo id"  in Groovy
    Works as a kick starter sample, notice the following differences: The bean
    attribute "script-interfaces" is not supported by Spring for Groovy scripts,
    instead one must declare the implemented interfaces in the Groovy source code
    itself. The Groovy code also needs to declare types that are compatible with the
    implemented interfaces. (So, in summary, coercion from loosely typed script
    methods to formal interface is slightly less flexible than for BeanShell, but
    code is clearer and allows better IDE integration with existing Java code.)

---
+++ src/main/resources/vtk/beans/standard-extensions/vshell/commands/repo/id.groovy
@@ -0,0 +1,11 @@
+package vtk.shell.vshell
+
+class IdCommand implements VCommand {
+    String getDescription() { "Displays repository ID" }
+
+    String getUsage() { "repo id" }
+
+    void execute(VShellContext context, Map args, PrintStream out) {
+        out.println(context.get("context").getBean("repository").getId())
+    }
+}
