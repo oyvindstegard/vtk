1680451ecff7445b4916ff36d2813fcf0e51a651
Øyvind Stegard <oyvind.stegard@usit.uio.no>
2017-08-03 20:31:28 +0200

    VShell: implement "repo cache" in Groovy

---
+++ src/main/resources/vtk/beans/standard-extensions/vshell/commands/repo/cache.groovy
@@ -0,0 +1,29 @@
+package vtk.shell.vshell
+
+class CacheCommand implements VCommand {
+    String getDescription() {
+        "Operate on the resource cache"
+    }
+
+    String getUsage() {
+        "repo cache <command:string>"
+    }
+
+    void execute(VShellContext context, Map args, PrintStream out) {
+        def cache = context.get("context").getBean("repository.cache")
+
+        switch (args.command) {
+          case "clear":
+            cache.clear()
+            break
+          case "size":
+            out.println(cache.size())
+            break
+          case "dump":
+            cache.dump(out)
+            break
+          default:
+            out.println("Unknown cache command: ${args.command}, available commands: clear, size, dump")
+        }
+    }
+}
