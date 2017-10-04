5c802bdb092a8ab3b41520ea90e6ce86bc41e548
Øyvind Hatland <oyvind.hatland@usit.uio.no>
2017-10-04 15:37:21 +0200

    VTK-5112 - add link to edit in login-manage component (assertion for ajaxeditor have to be checked again as it's not checked in Service.contructURL(), not sure why)

--- src/main/java/vtk/web/decorating/components/LoginManageComponent.java
+++ src/main/java/vtk/web/decorating/components/LoginManageComponent.java
@@ -32,12 +32,14 @@ package vtk.web.decorating.components;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Required;
 
 import vtk.repository.Path;
 import vtk.repository.Repository;
+import vtk.repository.RepositoryAction;
 import vtk.repository.Resource;
 import vtk.security.Principal;
 import vtk.web.RequestContext;
@@ -46,6 +48,7 @@ import vtk.web.decorating.DecoratorResponse;
 import vtk.web.display.listing.ListingPager;
 import vtk.web.service.Service;
 import vtk.web.service.URL;
+import vtk.web.service.WebAssertion;
 
 public class LoginManageComponent extends ViewRenderingDecoratorComponent {
 
@@ -55,6 +58,7 @@ public class LoginManageComponent extends ViewRenderingDecoratorComponent {
     private Service logoutService;
     private boolean displayOnlyIfAuth = false;
     private boolean displayAuthUser = false;
+    private WebAssertion ajaxEditorExistsAssertion;
 
     @Override
     protected void processModel(Map<String, Object> model, DecoratorRequest request, DecoratorResponse response)
@@ -78,34 +82,41 @@ public class LoginManageComponent extends ViewRenderingDecoratorComponent {
         Map<String, URL> options = new LinkedHashMap<>();
 
         try {
-            if (principal == null && !displayOnlyIfAuth) { // Not logged in (unauthenticated)
-                URL loginURL = ListingPager.removePagerParms(URL.create(request.getServletRequest()));
-                loginURL.removeParameter("authTarget");
-                loginURL.addParameter("authTarget", request.getServletRequest().getScheme());
-                options.put("login", loginURL);
+            if (principal == null && !displayOnlyIfAuth) { // Not logged in - Unauthenticated
+                this.putLoginURL(options, request);
                 this.putAdminURL(options, resource, request);
-            }
-            else if (principal != null) { // Logged in (authenticated)
+            } else if (principal != null) { // Logged in - Authenticated)
                 if (displayAuthUser) {
                     options.put("principal-desc", null);
                 }
                 this.putAdminURL(options, resource, request);
-                URL logoutURL = logoutService.urlConstructor(requestContext.getRequestURL())
-                        .withResource(resource)
-                        .withPrincipal(principal)
-                        .constructURL();
-                options.put("logout", logoutURL);
+                this.putEditURL(options, resource, request, principal, repository);
+                this.putLogoutURL(options, resource, principal, requestContext);
             }
         }
         catch (Exception e) { }
 
         model.put("options", options);
     }
+    
+    private void putLoginURL(Map<String, URL> options, DecoratorRequest request) throws Exception {
+        URL loginURL = ListingPager.removePagerParms(URL.create(request.getServletRequest()));
+        loginURL.removeParameter("authTarget");
+        loginURL.addParameter("authTarget", request.getServletRequest().getScheme());
+        options.put("login", loginURL);
+    }
+    
+    private void putLogoutURL(Map<String, URL> options, Resource resource, Principal principal, RequestContext requestContext) throws Exception {
+        URL logoutURL = logoutService.urlConstructor(requestContext.getRequestURL())
+                .withResource(resource)
+                .withPrincipal(principal)
+                .constructURL();
+        options.put("logout", logoutURL);
+    }
 
     private void putAdminURL(Map<String, URL> options, Resource resource, DecoratorRequest request) throws Exception {
         Service adminService = this.alternativeLoginServices.get("admin");
         if (adminService != null) {
-            
             URL adminURL = adminService.urlConstructor(URL.create(request.getServletRequest()))
                     .withURI(resource.getURI())
                     .constructURL();
@@ -116,6 +127,48 @@ public class LoginManageComponent extends ViewRenderingDecoratorComponent {
                 options.put("admin", adminURL);
             }
         }
+    }
+
+    private void putEditURL(Map<String, URL> options, Resource resource, DecoratorRequest request, Principal principal, Repository repository) throws Exception {
+        URL editURL = null;
+        Service editService = null;
+        
+        if(ajaxEditorExistsAssertion.matches(request.getServletRequest(), resource, principal)) { // Need to check this here also, as not checked in Service.constructURL for some reason
+            editService = this.alternativeLoginServices.get("ajaxeditor.service");
+            if (editService != null) editURL = editContructURL(editURL, editService, resource, request, principal);
+        } else {
+            editService = this.alternativeLoginServices.get("structuredResources.editService");
+            if (editService != null) editURL = editContructURL(editURL, editService, resource, request, principal);
+            
+            editService = this.alternativeLoginServices.get("editor.service");
+            if (editService != null) editURL = editContructURL(editURL, editService, resource, request, principal);
+        }
+        
+        if(editURL != null) {
+            boolean isWriteAuthorizedNotLockedByOther = repository.isAuthorized(resource, RepositoryAction.READ_WRITE, principal, true);
+            if(isWriteAuthorizedNotLockedByOther) {
+                if (resource.isCollection()) {
+                    options.put("edit-collection", editURL);
+                } else {
+                    options.put("edit", editURL);
+                }
+            }
+        }
+    }
+    
+    private URL editContructURL(URL editURL, Service editService, Resource resource, DecoratorRequest request, Principal principal) {
+        if(editURL == null) {
+            try {
+                editURL = editService.urlConstructor(URL.create(request.getServletRequest()))
+                              .withURI(resource.getURI())
+                              .withResource(resource)
+                              .withPrincipal(principal)
+                              .constructURL();
+            } catch (Exception e) {
+                // URL not created because of assertions
+            }
+        }
+        return editURL;
     }
 
     public void setAlternativeLoginServices(Map<String, Service> alternativeLoginServices) {
@@ -151,6 +204,10 @@ public class LoginManageComponent extends ViewRenderingDecoratorComponent {
 
     public void setDisplayAuthUser(boolean displayAuthUser) {
         this.displayAuthUser = displayAuthUser;
+    }
+
+    public void setAjaxEditorExistsAssertion(WebAssertion ajaxEditorExistsAssertion) {
+        this.ajaxEditorExistsAssertion = ajaxEditorExistsAssertion;
     }
 
 }
