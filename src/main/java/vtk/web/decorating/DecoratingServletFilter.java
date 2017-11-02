c99b6996684006e426643aea2e61bcc9bfcf43fa
Gorm Paulsen <gorm.paulsen@usit.uio.no>
2017-11-02 13:24:36 +0100

    [VTK-5407] Invoke document components when x-prevent-decorating set
    By default, invoke components in document even if flag
    'x-prevent-decorating' is specified. Add a second flag
    'x-prevent-parsing' which can be specified to also prevent document
    component parsing.
    
    (cherry picked from commit f565f3ab9a6d4f83b9be72afbd46a9c8eba40420)

--- src/main/java/vtk/web/decorating/DecoratingServletFilter.java
+++ src/main/java/vtk/web/decorating/DecoratingServletFilter.java
@@ -33,9 +33,11 @@ package vtk.web.decorating;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
@@ -45,6 +47,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import vtk.text.html.HtmlNodeFilter;
 import vtk.text.html.HtmlPageParser;
 import vtk.web.RequestContext;
@@ -53,23 +58,28 @@ import vtk.web.servlet.AbstractServletFilter;
 import vtk.web.servlet.VTKServlet;
 
 public class DecoratingServletFilter extends AbstractServletFilter {
+    private static Logger logger = LoggerFactory.getLogger(DecoratingServletFilter.class);
+    
     private HtmlPageParser htmlParser;
     private List<HtmlNodeFilterFactory> filters;
     private DecorationResolver resolver;
+    private String preventParsingParameter;
     private String preventDecoratingParameter;
-    private Map<String, String> staticHeaders;
+    private Optional<Map<String, String>> staticHeaders;
     private long maxSize;
     
     public DecoratingServletFilter(HtmlPageParser htmlParser,
             List<HtmlNodeFilterFactory> filters, DecorationResolver resolver,
             String preventDecoratingParameter,
+            String preventParsingParameter,
             Map<String, String> staticHeaders,
             long maxSize) {
         this.htmlParser = htmlParser;
         this.filters = new ArrayList<>(filters);
         this.resolver = resolver;
         this.preventDecoratingParameter = preventDecoratingParameter;
-        if (staticHeaders != null) this.staticHeaders = new HashMap<>(staticHeaders);
+        this.preventParsingParameter = preventParsingParameter;
+        this.staticHeaders = Optional.ofNullable(staticHeaders).map(hdrs -> new HashMap<>(hdrs));
         this.maxSize = maxSize;
     }
     
@@ -77,44 +87,78 @@ public class DecoratingServletFilter extends AbstractServletFilter {
     public void doFilter(HttpServletRequest request, HttpServletResponse response, 
             FilterChain chain) throws IOException, ServletException {
         
-        if (preventDecorating(request)) {
+        if ("true".equals(request.getAttribute(VTKServlet.INDEX_FILE_REQUEST_ATTRIBUTE))) {
             chain.doFilter(request, response);
             return;
         }
         
-        List<HtmlNodeFilter> htmlFilters = filters.stream()
+        boolean preventParsing = findParameter(request, preventParsingParameter).orElse(false);
+        boolean preventDecorating = findParameter(request, preventDecoratingParameter).orElse(false);
+        
+        logger.debug("Decorating {}: prevention parameters: {}={}, {}={}", 
+                request.getRequestURI(), 
+                preventParsingParameter, preventParsing, 
+                preventDecoratingParameter, preventDecorating);
+        
+        if (preventParsing && preventDecorating) {
+            logger.debug("Not decorating {}: prevented by parameters", request.getRequestURI()); 
+            chain.doFilter(request, response);
+            return;
+        }
+        
+        List<HtmlNodeFilter> htmlFilters = preventParsing ? Collections.emptyList() : 
+            filters.stream()
                 .map(factory -> factory.nodeFilter(request))
                 .collect(Collectors.toList());
         
+        logger.debug("HTML parse filters on request {}: {}", 
+                request.getRequestURI(), htmlFilters); 
+        
+        DecorationResolver overridingResolver  = overridingResolver(
+                preventDecorating ? Optional.empty() : Optional.of(resolver),
+                ! preventParsing);
+        
         response = new DecoratingServletResponse(
-                request, response, resolver, htmlParser, 
+                request, response, overridingResolver, htmlParser, 
                 htmlFilters, maxSize);
-        if (staticHeaders != null) {
-            response = new StaticHeadersResponse(response, staticHeaders);
+        
+        if (staticHeaders.isPresent()) {
+            logger.debug("Decorating request {} wih static headers: {}", 
+                    request.getRequestURI(), staticHeaders); 
+
+            response = new StaticHeadersResponse(response, staticHeaders.get());
         }
         chain.doFilter(request, response);
         response.flushBuffer();
     }
-
-    private boolean preventDecorating(HttpServletRequest request) {
-        if ("true".equals(request.getAttribute(VTKServlet.INDEX_FILE_REQUEST_ATTRIBUTE))) {
-            return true;
+    
+    /**
+     * Checks request if parameter exists, or service attributes for {@code parameter=true}
+     */
+    private Optional<Boolean> findParameter(HttpServletRequest request, String parameter) {
+        if (parameter == null) return Optional.empty();
+        String requestParameter = request.getParameter(parameter);
+        if (requestParameter != null) {
+            if ("true".equals(requestParameter) || "".equals(requestParameter))
+                return Optional.of(true);
         }
-
-        if (preventDecoratingParameter == null) return false;
-        if (request.getParameter(preventDecoratingParameter) != null)
-            return true;
         Service service = RequestContext.getRequestContext(request).getService();
         while (service != null) {
-            if ("true".equals(service.getAttribute(preventDecoratingParameter))) {
-                return true;
+            Object attribute = service.getAttribute(parameter);
+            if (attribute != null) {
+                if ("true".equals(attribute) || "".equals(attribute)) {
+                    return Optional.of(true);
+                }
+                return Optional.of(false);
             }
             else {
                 service = service.getParent();
             }
         }
-        return false;
+        return Optional.empty();
     }
+    
+    
 
     private static class StaticHeadersResponse extends HttpServletResponseWrapper {
         private Map<String, String> staticHeaders;
@@ -157,4 +201,48 @@ public class DecoratingServletFilter extends AbstractServletFilter {
         }        
     }
     
+    private DecorationResolver overridingResolver(final Optional<DecorationResolver> resolver, 
+            final boolean parse) {
+        return new DecorationResolver() {
+            @Override
+            public DecorationDescriptor resolve(HttpServletRequest request,
+                    HttpServletResponse response) {
+                
+                Optional<DecorationDescriptor> resolved = 
+                        resolver.map(r -> r.resolve(request, response));
+                
+                return new DecorationDescriptor() {
+                    @Override
+                    public boolean decorate() {
+                        return resolved.map(descriptor -> descriptor.decorate())
+                                .orElse(parse);
+                    }
+                    @Override
+                    public boolean tidy() {
+                        return resolved.map(descriptor -> descriptor.tidy()).orElse(false);
+                        
+                    }
+                    @Override
+                    public boolean parse() {
+                        return resolved.map(descriptor -> descriptor.parse())
+                                .orElse(parse);
+                    }
+
+                    @Override
+                    public List<Template> getTemplates() {
+                        return resolved.map(descriptor -> descriptor.getTemplates())
+                                .orElse(Collections.emptyList());
+                    }
+
+                    @Override
+                    public Map<String, Object> getParameters(
+                            Template template) {
+                        return resolved.map(descriptor -> descriptor.getParameters(template))
+                                .orElse(Collections.emptyMap());
+                    }
+                };
+            }
+            
+        };
+    }
 }
